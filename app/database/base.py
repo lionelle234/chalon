28ce84d33ae58becc6d52b0e079f3cbc5f1a406c
lionelle234 <jegrash55@tutanota.com>
2021-04-29 17:00:07 -0300

    database

--- app/database/base.py
+++ app/database/base.py
@@ -6,22 +6,18 @@ from sqlalchemy.orm import sessionmaker
 
 
 db = dict(driverDB="{ODBC Driver 17 for SQL Server}",
-          serverDB="DESKTOP-KSACHTT\SQLEXPRESS",
+          serverDB="localhost",
           portDB="1433",
-          userDB="adminV",
-          database="CHAL",
-          passwordDB="CHAOSSHIT2")
+          userDB="localhost")
 
 conn_str = (f"Driver={db['driverDB']}"
             f";Server={db['serverDB']}"
             f";Port={db['portDB']}"
-            f";Database={db['database']}"
             f";UID={db['userDB']}"
-            f";PWD={db['passwordDB']}"
             ";TrustServerCertificate=no")
 
 quoted_conn_str = urllib.parse.quote_plus(conn_str)
-engine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted_conn_str))
+engine = create_engine("mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4")
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
